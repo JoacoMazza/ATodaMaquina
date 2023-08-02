c3b7e77abe5925458e84f5a3ba7842944727f5db
Joaquin Mazza <mazzajoaquin22@gmail.com>
2023-08-02 16:55:56 -0300

    agregado login logout register

--- project/apps/Home/urls.py
+++ project/apps/Home/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+from django.views.generic import TemplateView
+
+from . import views
 
 from .views import home
 
 app_name = "Home"
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path('login/', views.login_request, name="login"),
+    path('logout/', LogoutView.as_view(template_name="Home/logout.html"), name="logout"),
+    path('about/', TemplateView.as_view(template_name="Home/about.html"), name="about"),
+    path('register/', views.register, name="register"),
 
 ]
 
